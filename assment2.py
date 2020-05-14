d9b85587f97c4dd0ba8e1a31e47fa1d03f9b349f
rgmishan <mishanregmi@gmail.com>
2020-05-14 16:18:58 +0530

    class assignment 2 updated

--- assment2.py
+++ assment2.py
@@ -22,10 +22,49 @@ print(cou) # print 2 by counting "class" character in string
 fin = myString.find("on")
 print(fin) # print 23 by getting the index of "on" from string
 #5
-myString2 = "Joining"
+myString2 = " Joining this string"
 jon = "+".join(myString2)
 print(jon) # join all character of string with (+)
 #6
 myString3 = "     Strip Me     "
 stp = myString3.strip()
 print(stp) # Remove the white space from the string
+
+print("\nTask 2")
+
+print("\nIndex Operator")
+
+result = myString.index('to Python')
+print("Substring 'to Python' is in:", result, "position.")
+
+print("\nLogical Operator")
+str1 = "Python"
+str2 = "Programming"
+
+if str1 == "Python" or str2 == "Prog" :
+    print("One Condition Match")
+
+if str1 == "Python" and str2 == "Prog" :
+    print("Both Condition Match")
+
+print("\nMembership Operator")
+
+if "Welcome" in myString:
+     print("(Welcome) found in myString")
+if "Mishan" not in myString:
+     print("(Mishan) not found in myString")
+
+print("\nIdentity Operator")
+print(type(myString))
+num = 5
+num1 = "5"
+num2 = 5.0
+num3 = True
+print(type(num))
+print(type(num1))
+print(type(num2))
+print(type(num3))
+
+print("\nConcatenating Operator")
+new = myString + myString2
+print(new)
