827551bfe684976c76ef07851ca2e1164a824511
rgmishan <mishanregmi@gmail.com>
2020-05-16 22:31:47 +0530

    practice 5

---
+++ practice program/pra5.py
@@ -0,0 +1,14 @@
+#Question 5: Given a list of numbers, return True if first and last number of a list is same
+
+list1 = [1,2,3,4,5,1]
+list2 = [1,2,3,4,5,6]
+
+if list1[0] == list1[-1]:
+ print("result is:", True)
+else:
+ print("result is:", False)
+
+if list2[0] == list2[-1]:
+ print("result is:", True)
+else:
+ print("result is:", False)
