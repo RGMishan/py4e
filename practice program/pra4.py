7a8e33283dc7ca0fec7afda93fb50419c37a3410
rgmishan <mishanregmi@gmail.com>
2020-05-16 22:30:26 +0530

    practice program 4

---
+++ practice program/pra4.py
@@ -0,0 +1,8 @@
+#Question 4: Given a string and an integer number n, remove characters from a
+#string starting from zero up to n and return a new string
+
+string = "My name is Mishan Regmi"
+n = 5
+
+new_str = string[0:n]
+print(new_str)
