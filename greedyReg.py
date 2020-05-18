940c5f1338cd99d9e613b5a4117c589fcc623f25
rgmishan <mishanregmi@gmail.com>
2020-05-19 00:03:23 +0530

    minor change

--- greedyReg.py
+++ greedyReg.py
@@ -1,6 +1,11 @@
 # 1 Finding using simple way
 # 2 Greedy Matching With Regex
 
+# ^ Begin with
+# . Any Character after that
+# \S until there is white space chatacter
+# + one or more character greedy
+# ? dont be greedy 
 import re
 
 x = 'From: Using the : character Fro: me also'
@@ -10,7 +15,10 @@ print(m)
 n = re.findall('^Fro\S+:',x) #Start from F and inclue until you get whitespace
 print(n)
 
-y = re.findall('^F.+:',x) 
+o = re.findall('^F.+:',x) 
 # '^' means F should be at beginning '.' means any character '+' means followed by 
 # any character one or more until you found the last ':' symbol (LAST SYMBOL)
-print(y)
+print(o)
+
+p = re.findall('^F.+?:',x)  # This code is withput being greedy
+print(p)
