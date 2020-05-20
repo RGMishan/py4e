6f0a4912e107798ffd263d75c85d0285b4c98703
RGMishan <mishanregmi@gmail.com>
2020-05-20 22:35:32 +0530

    csv format

---
+++ functionFileDictionary/week1/csv.py
@@ -0,0 +1,13 @@
+fileconnection = open('grades.txt', 'r')
+lines = fileconnection.readlines()
+for lin in lines[0:]:
+    print(lin.strip())
+print("------------------")
+header = lines[0]
+field_name = header.strip().split(',')
+print(field_name)
+
+for row in lines[1:]:
+    vals = row.strip().split(',')
+    if vals[2] != '':
+       print("{}:    {};     {}".format(vals[0],vals[1],vals[2]))
