48294eee069a1bea29a7a4e4659f7501e4f34527
RGMishan <mishanregmi@gmail.com>
2020-05-20 21:28:13 +0530

    updated readwritefiles

--- functionFileDictionary/week1/readWriteFile.py
+++ functionFileDictionary/week1/readWriteFile.py
@@ -4,7 +4,7 @@ f= open("test.txt","w+")
 
 for number in range(13):
     square = number * number
-    ''' 
+    '''
     f.write(str(square))
     f.write("\n")
     '''
@@ -19,3 +19,20 @@ new_file = open("test.txt", "r")
 #print(new_file.read(10))
 print(new_file.read()[:10])
 
+#using with for files
+with open('test.txt', 'r') as re:
+    for line in re:
+        print(line)
+
+#being more general with with
+'''
+file = test.txt
+with open(file, 'r') as re:
+    re.read()
+    re.readline()
+    re.readlines()
+    for line in re:
+        do something with each line
+
+WE CAN DO THIS WITH WRITE ALSO
+'''
