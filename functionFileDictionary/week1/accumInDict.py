4a75c7ad9015f2ee712b3dfa270b8c9097b35178
RGMishan <mishanregmi@gmail.com>
2020-05-25 12:47:45 +0530

    accum modified

--- functionFileDictionary/week1/accumInDict.py
+++ functionFileDictionary/week1/accumInDict.py
@@ -9,5 +9,8 @@ for c in txt:
         x[c] = 0 #character not seen so initialized as zero
     x[c] = x[c] + 1 # not seen so increment in counter
 
-print('The no of m in file is', str(x['m']) ,"times.")
+print('The no of m in file is', str(x['m']) ,"times.") #passing m in the c value
 print('The no of m in file is', str(x['a']) ,"times.")
+
+for c in x.keys():
+    print("letter",c, "occur", str(x[c]), "times")
