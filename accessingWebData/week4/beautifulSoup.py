17b4b6dfe4f515d571b58b4fc51ec6ac601e03e7
RGMishan <mishanregmi@gmail.com>
2020-05-27 22:35:30 +0530

    beautiful soup worked

--- accessingWebData/week4/beautifulSoup.py
+++ accessingWebData/week4/beautifulSoup.py
@@ -1,13 +1,15 @@
 #using beautiful soup to parse xml and HTML
 
 import urllib.request, urllib.parse, urllib.error
-import beautifulSoup
+from bs4 import BeautifulSoup
 
 url = input("Enter the URL:")
 html = urllib.request.urlopen(url).read()
-soup = beautifulSoup(html, 'html.parser')
+soup = BeautifulSoup(html, 'html.parser')
 
 #Retrive all the anchor tags
 tags = soup('a')
 for tag in tags:
  print(tag.get('href', None))
+
+ #used beautiful soup to bring the list of anchor tag from the HTML
