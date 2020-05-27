12d253ec29482ef150614246598647c9a7036ee1
rgmishan <mishanregmi@gmail.com>
2020-05-27 19:17:07 +0530

    socket shortcut completed

--- accessingWebData/week4/shortcutSocket.py
+++ accessingWebData/week4/shortcutSocket.py
@@ -4,7 +4,16 @@ import urllib.request, urllib.parse, urllib.error
 fhand = urllib.request.urlopen("http://data.pr4e.org/romeo.txt")
 #above line is similar to when we use open file dont read data but go to url and fetch the file and comes
 
+#getting info from the file to run the second code this part should be comment
 for line in fhand:
  print(line.decode().strip())
 
 # now treating like a file
+
+counts = dict()
+for line in fhand:
+ words = line.decode().strip()
+ for word in words:
+  counts[word] = counts.get(word, 0) + 1
+
+print(counts)
